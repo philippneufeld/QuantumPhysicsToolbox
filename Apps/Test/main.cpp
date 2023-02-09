d451232229c57e946eb2ce0028f932f0b10b81f4
Philipp Neufeld <philipp.neufeld@t-online.de>
2023-02-09 20:07:41 +0100

    Project skeleton

---
+++ Apps/Test/main.cpp
@@ -0,0 +1,11 @@
+// Philipp Neufeld, 2023
+
+#include <iostream>
+#include <Eigen/Dense>
+#include <hdf5.h>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello world" << std::endl;
+    return 0;
+}
---
+++ QPT/Platform.h
@@ -0,0 +1,8 @@
+// Philipp Neufeld, 2023
+
+#ifndef QPT_PLATFORM_H_
+#define QPT_PLATFORM_H_
+
+
+
+#endif
